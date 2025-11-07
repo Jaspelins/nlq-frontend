65a00e367540c162aec36ffac6a1434780b8b303
jaspechase <jaspechase@gmail.com>
2025-11-07 18:32:20 +0000

    chore: reset project to start from scratch

--- src/app/api/master/link-user-to-company/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function POST(request: Request) {
-  const { userId, companyId } = await request.json();
-
-  // Add your logic to link the user to the company in the database here
-
-  return NextResponse.json({ message: `User ${userId} linked to company ${companyId} successfully` });
-}
